0134628c64f81d5ad132896b720d90f75151cbed
Nikhil John <nikjohn@deloitte.com>
2015-07-31 13:19:18 +0530

    Committing files

---
+++ backgroundAnime.js
@@ -0,0 +1,343 @@
+'use strict';
+
+// stats.js - http://github.com/mrdoob/stats.js
+var Stats = function() {
+    var l = Date.now(),
+        m = l,
+        g = 0,
+        n = Infinity,
+        o = 0,
+        h = 0,
+        p = Infinity,
+        q = 0,
+        r = 0,
+        s = 0,
+        f = document.createElement("div");
+    f.id = "stats";
+    f.addEventListener("mousedown", function(b) {
+        b.preventDefault();
+        t(++s % 2)
+    }, !1);
+    f.style.cssText = "width:80px;opacity:0.9;cursor:pointer";
+    var a = document.createElement("div");
+    a.id = "fps";
+    a.style.cssText = "padding:0 0 3px 3px;text-align:left;background-color:#002";
+    f.appendChild(a);
+    var i = document.createElement("div");
+    i.id = "fpsText";
+    i.style.cssText = "color:#0ff;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px";
+    i.innerHTML = "FPS";
+    a.appendChild(i);
+    var c = document.createElement("div");
+    c.id = "fpsGraph";
+    c.style.cssText = "position:relative;width:74px;height:30px;background-color:#0ff";
+    for (a.appendChild(c); 74 > c.children.length;) {
+        var j = document.createElement("span");
+        j.style.cssText = "width:1px;height:30px;float:left;background-color:#113";
+        c.appendChild(j)
+    }
+    var d = document.createElement("div");
+    d.id = "ms";
+    d.style.cssText = "padding:0 0 3px 3px;text-align:left;background-color:#020;display:none";
+    f.appendChild(d);
+    var k = document.createElement("div");
+    k.id = "msText";
+    k.style.cssText = "color:#0f0;font-family:Helvetica,Arial,sans-serif;font-size:9px;font-weight:bold;line-height:15px";
+    k.innerHTML = "MS";
+    d.appendChild(k);
+    var e = document.createElement("div");
+    e.id = "msGraph";
+    e.style.cssText = "position:relative;width:74px;height:30px;background-color:#0f0";
+    for (d.appendChild(e); 74 > e.children.length;) j = document.createElement("span"), j.style.cssText = "width:1px;height:30px;float:left;background-color:#131", e.appendChild(j);
+    var t = function(b) {
+        s = b;
+        switch (s) {
+            case 0:
+                a.style.display =
+                    "block";
+                d.style.display = "none";
+                break;
+            case 1:
+                a.style.display = "none", d.style.display = "block"
+        }
+    };
+    return {
+        REVISION: 11,
+        domElement: f,
+        setMode: t,
+        begin: function() {
+            l = Date.now()
+        },
+        end: function() {
+            var b = Date.now();
+            g = b - l;
+            n = Math.min(n, g);
+            o = Math.max(o, g);
+            k.textContent = g + " MS (" + n + "-" + o + ")";
+            var a = Math.min(30, 30 - 30 * (g / 200));
+            e.appendChild(e.firstChild).style.height = a + "px";
+            r++;
+            b > m + 1E3 && (h = Math.round(1E3 * r / (b - m)), p = Math.min(p, h), q = Math.max(q, h), i.textContent = h + " FPS (" + p + "-" + q + ")", a = Math.min(30, 30 - 30 * (h / 100)), c.appendChild(c.firstChild).style.height =
+                a + "px", m = b, r = 0);
+            return b
+        },
+        update: function() {
+            l = this.end()
+        }
+    }
+};
+
+/**
+ * @author alteredq / http://alteredqualia.com/
+ * @author mr.doob / http://mrdoob.com/
+ */
+
+var Detector = {
+
+	canvas: !! window.CanvasRenderingContext2D,
+	webgl: ( function () { try { var canvas = document.createElement( 'canvas' ); return !! ( window.WebGLRenderingContext && ( canvas.getContext( 'webgl' ) || canvas.getContext( 'experimental-webgl' ) ) ); } catch ( e ) { return false; } } )(),
+	workers: !! window.Worker,
+	fileapi: window.File && window.FileReader && window.FileList && window.Blob,
+
+	getWebGLErrorMessage: function () {
+
+		var element = document.createElement( 'div' );
+		element.id = 'webgl-error-message';
+		element.style.fontFamily = 'monospace';
+		element.style.fontSize = '13px';
+		element.style.fontWeight = 'normal';
+		element.style.textAlign = 'center';
+		element.style.background = '#fff';
+		element.style.color = '#000';
+		element.style.padding = '1.5em';
+		element.style.width = '400px';
+		element.style.margin = '5em auto 0';
+
+		if ( ! this.webgl ) {
+
+			element.innerHTML = window.WebGLRenderingContext ? [
+				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' ) : [
+				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join( '\n' );
+
+		}
+
+		return element;
+
+	},
+
+	addGetWebGLMessage: function ( parameters ) {
+
+		var parent, id, element;
+
+		parameters = parameters || {};
+
+		parent = parameters.parent !== undefined ? parameters.parent : document.body;
+		id = parameters.id !== undefined ? parameters.id : 'oldie';
+
+		element = Detector.getWebGLErrorMessage();
+		element.id = id;
+
+		parent.appendChild( element );
+
+	}
+
+};
+
+// browserify support
+if ( typeof module === 'object' ) {
+
+	module.exports = Detector;
+
+}
+
+
+if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
+
+			var container, stats;
+			var camera, scene, renderer, particles, geometry, materials = [], parameters, i, h, color, sprite, size;
+			var mouseX = 0, mouseY = 0;
+
+			var windowHalfX = window.innerWidth / 2;
+			var windowHalfY = window.innerHeight / 2;
+
+			init();
+			animate();
+
+			function init() {
+
+				container = document.createElement( 'div' );
+				document.body.appendChild( container );
+
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
+				camera.position.z = 1000;
+
+				scene = new THREE.Scene();
+				scene.fog = new THREE.FogExp2( 0x000000, 0.0008 );
+
+				geometry = new THREE.Geometry();
+
+
+
+				var sprite1 = THREE.ImageUtils.loadTexture( "assets/images/Object1.svg" );
+				var sprite2 = THREE.ImageUtils.loadTexture( "assets/images/Object2.svg" );
+				var sprite3 = THREE.ImageUtils.loadTexture( "assets/images/Object3.svg" );
+				var sprite4 = THREE.ImageUtils.loadTexture( "assets/images/Object4.svg" );
+				var sprite5 = THREE.ImageUtils.loadTexture( "assets/images/Object1.svg" );
+
+				// sprite1 = THREE.ImageUtils.loadTexture( "resources/assets/leaf.jpg" );
+				// sprite2 = THREE.ImageUtils.loadTexture( "resources/assets/leaf.jpg" );
+				// sprite3 = THREE.ImageUtils.loadTexture( "resources/assets/leaf.jpg" );
+				// sprite4 = THREE.ImageUtils.loadTexture( "resources/assets/leaf.jpg" );
+				// sprite5 = THREE.ImageUtils.loadTexture( "resources/assets/leaf.jpg" );
+
+				for ( i = 0; i < 15; i ++ ) {
+
+					var vertex = new THREE.Vector3();
+
+					vertex.x = Math.random() * 2000 - 1000;
+					vertex.y = Math.random() * 2000 - 1000;
+					vertex.z = Math.random() * 2000 - 1000;
+
+					geometry.vertices.push( vertex );
+
+				}
+
+				parameters = [ [ [1.0, 0.2, 0.5], sprite2, 70 ],
+							   [ [0.95, 0.1, 0.5], sprite3, 85 ],
+							   [ [0.90, 0.05, 0.5], sprite1, 70 ],
+							   [ [0.85, 0, 0.5], sprite5, 28 ],
+							   [ [0.80, 0, 0.5], sprite4, 25 ],
+							   ];
+
+				for ( i = 0; i < parameters.length; i ++ ) {
+
+					color  = parameters[i][0];
+					sprite = parameters[i][1];
+					size   = parameters[i][2];
+
+					materials[i] = new THREE.PointCloudMaterial( { size: size, map: sprite, blending: THREE.AdditiveBlending, depthTest: false, transparent : true } );
+					//materials[i].color.setHSL( color[0], color[1], color[2] );
+
+					particles = new THREE.PointCloud( geometry, materials[i] );
+
+					particles.rotation.x = Math.random() * 6;
+					particles.rotation.y = Math.random() * 6;
+					particles.rotation.z = Math.random() * 6;
+
+					scene.add( particles );
+
+				}
+
+				renderer = new THREE.WebGLRenderer();
+				renderer.setPixelRatio( window.devicePixelRatio );
+				renderer.setSize( window.innerWidth, window.innerHeight );
+				container.appendChild( renderer.domElement );
+
+				stats = new Stats();
+				stats.domElement.style.position = 'absolute';
+				stats.domElement.style.top = '0px';
+				container.appendChild( stats.domElement );
+
+				document.addEventListener( 'mousemove', onDocumentMouseMove, false );
+				document.addEventListener( 'touchstart', onDocumentTouchStart, false );
+				document.addEventListener( 'touchmove', onDocumentTouchMove, false );
+
+				//
+
+				window.addEventListener( 'resize', onWindowResize, false );
+
+			}
+
+			function onWindowResize() {
+
+				windowHalfX = window.innerWidth / 2;
+				windowHalfY = window.innerHeight / 2;
+
+				camera.aspect = window.innerWidth / window.innerHeight;
+				camera.updateProjectionMatrix();
+
+				renderer.setSize( window.innerWidth, window.innerHeight );
+
+			}
+
+			function onDocumentMouseMove( event ) {
+
+				mouseX = event.clientX - windowHalfX;
+				mouseY = event.clientY - windowHalfY;
+
+			}
+
+			function onDocumentTouchStart( event ) {
+
+				if ( event.touches.length === 1 ) {
+
+					event.preventDefault();
+
+					mouseX = event.touches[ 0 ].pageX - windowHalfX;
+					mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+				}
+
+			}
+
+			function onDocumentTouchMove( event ) {
+
+				if ( event.touches.length === 1 ) {
+
+					event.preventDefault();
+
+					mouseX = event.touches[ 0 ].pageX - windowHalfX;
+					mouseY = event.touches[ 0 ].pageY - windowHalfY;
+
+				}
+
+			}
+
+			//
+
+			function animate() {
+
+				requestAnimationFrame( animate );
+
+				render();
+				/*stats.update();*/
+
+			}
+
+			function render() {
+
+				var time = Date.now() * 0.00005;
+
+				camera.position.x += ( mouseX - camera.position.x ) * 0.05;
+				camera.position.y += ( - mouseY - camera.position.y ) * 0.05;
+
+				camera.lookAt( scene.position );
+
+				for ( i = 0; i < scene.children.length; i ++ ) {
+
+					var object = scene.children[ i ];
+
+					if ( object instanceof THREE.PointCloud ) {
+
+						object.rotation.y = time * ( i < 4 ? i + 1 : - ( i + 1 ) );
+
+					}
+
+				}
+
+				for ( i = 0; i < materials.length; i ++ ) {
+
+					color = parameters[i][0];
+
+					h = ( 360 * ( color[0] + time ) % 360 ) / 360;
+					//materials[i].color.setHSL( h, color[1], color[2] );
+
+				}
+
+				renderer.render( scene, camera );
+
+			}
