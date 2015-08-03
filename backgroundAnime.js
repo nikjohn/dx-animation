d858ef19694437b09f644627b7fac5abb2be61fd
Nikhil John <nikjohn@deloitte.com>
2015-08-03 12:52:33 +0530

    Some changes to background anime - changed the perspective camera setting so that cubes don't hit you on the face

--- backgroundAnime.js
+++ backgroundAnime.js
@@ -172,7 +172,7 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 				container = document.createElement( 'div' );
 				document.body.appendChild( container );
 
-				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 2000 );
+				camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 100, 2000 );
 				camera.position.z = 1000;
 
 				scene = new THREE.Scene();
